21f19945ea9a217d50e17f75cfef93bc5d6a912f
stevenTheDeve <steventhedeve@gmail.com>
2024-07-26 07:55:14 -0700

    Initial Commit

---
+++ SwiftfulSwiftUIinPractice/SwiftfulSwiftUIinPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulSwiftUIinPracticeApp.swift
+//  SwiftfulSwiftUIinPractice
+//
+//  Created by Estevan Hinojosa on 7/26/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulSwiftUIinPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
